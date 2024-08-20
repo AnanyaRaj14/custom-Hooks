ce8de9dbebccbd06756d5853c2c3ebe057fb16ff
Ananya Raj <ananyam748@gmail.com>
2024-08-20 20:03:57 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-
-import './App.css'
-import { useState } from 'react'
-import CurrencyConverter from './components/currency-converter'
+import "./App.css";
+import CurrencyConvertor from "./components/currency-converter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="min-h-screen bg-green-200 flex flex-col items-center justify-center">
-      hello ananya
-      <CurrencyConverter/>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <div className="container">
+        <CurrencyConvertor />
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
